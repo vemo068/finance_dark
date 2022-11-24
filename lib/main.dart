115f1848628a97f3eb5350763c1d5784fe912ead
vemo068 <66393949+vemo068@users.noreply.github.com>
2022-11-24 01:39:50 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FinanceDarkApp extends StatelessWidget {
+  const FinanceDarkApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      
+    );
+  }
+}
